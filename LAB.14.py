c26759a682dd5c9ae29d9ec8faf6f6d66b544ca3
TranThanhNhat113 <nhattran14925@gmail.com>
2024-08-30 21:08:10 +0700

    Update LAB.14.py

--- LAB.14.py
+++ LAB.14.py
@@ -5,5 +5,5 @@ Created on Tue Aug 27 19:15:39 2024
 @author: Student
 """
 
-result= 32**0,2 - (1/64)**-0,25 + (8/27)**(1/3)
-print(f"A= {result}")
+A = (32 ** 0.2) - ((1 / 64) ** (-0.25)) + ((8 / 27) ** (1 / 3))
+print(A)
