97699193cd55206cfd4dfce216278651d04bc332
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-09 15:37:34 +0700

    Update LAB.27.py

--- LAB.27.py
+++ LAB.27.py
@@ -6,20 +6,25 @@ Created on Fri Aug 30 21:25:36 2024
 """
 
 import math
-hinh = input("Nhập hình muốn tính chu vi (P) và diện tích (S) (vuông, chữ nhật, tròn): ")
-if hinh == 'vuông':
+hinh = input("Nhập hình muốn tính chu vi (P) và diện tích (S) (v-vuông, n-chữ nhật, t-tròn): ")
+if hinh == 'v':
     canh = float(input("Nhập độ dài cạnh (m): "))
     chu_vi = canh * 4
     dien_tich = canh ** 2
-    print(f"Chu vi hình vuông là P = {chu_vi} m\nDiện tích hình vuông là S = {dien_tich} m\u00B2")
-elif hinh == 'chữ nhật':
+    print(f"Chu vi hình vuông là P = {chu_vi}")
+    print(f"Diện tích hình vuông là S = {dien_tich}")
+elif hinh == 'n':
     dai = float(input("Nhập chiều dài (m): "))
     rong = float(input("Nhập chiều rộng (m): "))
     chu_vi = (dai + rong) * 2
     dien_tich = dai * rong
-    print(f"Chu vi hình chữ nhật là P = {chu_vi} m\nDiện tích hình chữ nhật là S = {dien_tich} m\u00B2")
-else:
+    print(f"Chu vi hình chữ nhật là P = {chu_vi}")
+    print(f"Diện tích hình chữ nhật là S = {dien_tich}")
+elif hinh == "t" :
     r = float(input("Nhập độ dài bán kính r (m): "))
     chu_vi = 2 * r * math.pi
     dien_tich = (r ** 2) * math.pi
-    print(f"Chu vi hình tròn là P = {chu_vi} m\nDiện tích hình tròn là S = {dien_tich} m\u00B2")
+    print(f"Chu vi hình tròn là P = {chu_vi}")
+    print(f"Diện tích hình tròn là S = {dien_tich}") 
+else:
+    print("Loại hình không hợp lệ. Vui lòng nhập 'v', 'n', hoặc 't'.")
