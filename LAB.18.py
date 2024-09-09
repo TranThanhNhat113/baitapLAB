b68934e10538db154d911b5a467e88a486bc3e5a
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-09 14:31:43 +0700

    Update LAB.18.py

--- LAB.18.py
+++ LAB.18.py
@@ -5,14 +5,12 @@ Created on Fri Aug 30 21:20:49 2024
 @author: PC
 """
 
-gio_mot = input("Nhập vào giờ thứ nhất (giờ,phút,giây): ")
-gio_hai = input("Nhập vào giờ thứ hai (giờ,phút,giây): ")
-h1, p1, s1 = map(int, gio_mot.split(','))
-h2, p2, s2 = map(int, gio_hai.split(','))
-giay_mot = h1 * 3600 + p1 *60 + s1
-giay_hai = h2 * 3600 + p2 *60 + s2
-tong = giay_mot + giay_hai
-hieu = giay_mot - giay_hai
+h1, p1, s1 = map(int, input("Nhập giờ 1 (giờ,phút,giây): ").split(","))
+h2, p2, s2 = map(int, input("Nhập giờ 2 (giờ,phút,giây): ").split(","))
+giay_1 = h1 * 3600 + p1 *60 + s1
+giay_2 = h2 * 3600 + p2 *60 + s2
+tong = giay_1 + giay_2
+hieu = abs(giay_1 - giay_2)
 gio_tong = tong // 3600
 phut_tong = tong % 3600 // 60
 giay_tong = tong % 60
@@ -20,4 +18,4 @@ gio_hieu = hieu // 3600
 phut_hieu = hieu % 3600 // 60
 giay_hieu = hieu % 60
 print(f"Tổng hai giờ là: {gio_tong}giờ {phut_tong}phút {giay_tong}giây")
-print(f"Tổng hai giờ là: {gio_hieu}giờ {phut_hieu}phút {giay_hieu}giây")
+print(f"Hiệu hai giờ là: {gio_hieu}giờ {phut_hieu}phút {giay_hieu}giây")
