b94e096747f44e18b34e102f93968f23f096c5e3
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-09 15:14:07 +0700

    Update LAB.25.py

--- LAB.25.py
+++ LAB.25.py
@@ -5,8 +5,8 @@ Created on Fri Aug 30 21:23:22 2024
 @author: PC
 """
 
-ch = input("Nhập một chữ cái: ")
-if ch.isalpha() and len(ch) == 1:
-    print(ch.swapcase())
+chu = input("Nhập một chữ cái: ")
+if chu.isalpha() and len(chu) == 1:
+    print(chu.swapcase())
 else:
     print("Đầu vào không phải là một chữ cái.")
