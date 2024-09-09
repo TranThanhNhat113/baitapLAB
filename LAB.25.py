710d8bebb1897af2d5a016d5fad1a506c5ed7074
TranThanhNhat113 <nhattran14925@gmail.com>
2024-09-09 15:13:15 +0700

    Update LAB.25.py

--- LAB.25.py
+++ LAB.25.py
@@ -5,10 +5,8 @@ Created on Fri Aug 30 21:23:22 2024
 @author: PC
 """
 
-char = input("Nhập vào một chữ cái hoa hoặc thường: ")
-if char.islower():
-    chu_hoa = char.upper()
-    print(f"{char} là chữ thường đổi thành chữ hoa: {chu_hoa}")
+ch = input("Nhập một chữ cái: ")
+if ch.isalpha() and len(ch) == 1:
+    print(ch.swapcase())
 else:
-    chu_thuong = char.lower()
-    print(f"{char} là chữ hoa đổi thành chữ thường: {chu_thuong}")
+    print("Đầu vào không phải là một chữ cái.")
